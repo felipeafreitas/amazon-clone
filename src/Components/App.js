8a4760d4a69d25e91663c5cfa3756663abd6aede
Felipe Freitas <afelipe.freitas@gmail.com>
2021-03-25 21:03:41 -0300

    Half of first day done

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import Header from './Header.js'
+import Cart from './Cart.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,128 @@
+import React from 'react'
+import styled from 'styled-components'
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
+function Header() {
+    return (
+        <Container>
+            <HeaderLogo>
+                <img src={'https://www.nicepng.com/png/full/16-167642_amazon-logo-amazon-logo-white-text.png'} alt='amazon-logo' />
+            </HeaderLogo>
+
+            <HeaderOptionAddress>
+                <LocationOnIcon />
+
+                <HeaderOption>
+                    <OptionLineOne>Hello,</OptionLineOne>
+                    <OptionLineTwo>Select your adress</OptionLineTwo>
+                </HeaderOption>
+            </HeaderOptionAddress>
+
+            <HeaderSearch>
+                <HeaderSearchInput type='text' />
+
+                <HeaderSearchIconContainer>
+                    <SearchIcon />
+                </HeaderSearchIconContainer>
+            </HeaderSearch>
+
+            <HeaderNavitems>
+                <HeaderOption>
+                    <OptionLineOne>Hello, Sincopeiro</OptionLineOne>
+                    <OptionLineTwo>Account & Lists</OptionLineTwo>
+                </HeaderOption>
+
+                <HeaderOption>
+                    <OptionLineOne>Returns</OptionLineOne>
+                    <OptionLineTwo>& Orders</OptionLineTwo>
+                </HeaderOption>
+                    
+                <HeaderOptionCart>
+                    <ShoppingBasketIcon />
+                    <CartCount>5</CartCount>
+                </HeaderOptionCart>
+
+            </HeaderNavitems>
+        </Container>
+    )
+}
+
+export default Header
+
+const Container = styled.div`
+    height: 60px;
+    background-color: #0F1111;
+    display: flex;
+    align-items: center;
+    color: #fff;
+    justify-content: space-between;
+
+`
+const HeaderLogo = styled.div`
+    img {
+        width: 100px;
+        margin-left: 11px;
+    }
+`
+
+const HeaderOptionAddress = styled.div`
+    padding-left: 9px;
+    display: flex;
+    align-items: center;
+`
+
+const OptionLineOne = styled.div`
+`
+
+const OptionLineTwo = styled.div`
+    font-weight: 700;
+`
+
+const HeaderSearch = styled.div`
+    display: flex;
+    flex-grow: 1;
+    height: 40px;
+    border-radius: 4px;
+    overflow: hidden;
+    margin-left: 4px;
+    background-color: white;
+
+    :focus-within {
+        box-shadow: 0 0 0 3px #F90;
+
+    }
+`
+const HeaderSearchInput = styled.input`
+    flex-grow: 1;
+    border: 0;
+    :focus {
+        outline: none;
+    }
+
+`
+const HeaderSearchIconContainer = styled.div`
+    background-color: #febd69;
+    width: 45px;
+    color: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+const HeaderNavitems = styled.div`
+    display: flex;
+
+    
+`
+const HeaderOption = styled.div`
+    padding: 10px 9px;
+`
+const CartCount = styled.div`
+    padding-left: 4px;
+`
+const HeaderOptionCart = styled.div`
+    display: flex;
+    align-items: center;
+    padding-right: 9px;
+`
